7aaf81ae5a2c433c63bfe8bc1cfc408d7367da12
Conor Lyness <conorlyness@gmail.com>
2022-07-08 19:56:45 +0100

    cleanup home component class and error handle

--- bookBaseApp/client/src/app/components/home/home.component.ts
+++ bookBaseApp/client/src/app/components/home/home.component.ts
@@ -1,11 +1,8 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
-import { bookByGenre } from 'src/app/models/book.model';
 import { BookService } from 'src/app/services/book.service';
 import { FavouriteBooksService } from 'src/app/services/favourite-books.service';
 import { SnackbarService } from 'src/app/services/snackbar.service';
-import { FavouritesComponent } from '../favourites/favourites.component';
 
 @Component({
   selector: 'app-home',
@@ -27,13 +24,7 @@ export class HomeComponent implements OnInit {
 
   //TODO: change this so it calls get weeks popular books with a input from the user, via a dropdown
   ngOnInit(): void {
-    this.bookService.getWeeksPopularBooks('non fiction').subscribe({
-      next: (response) => {
-        console.log(response);
-        this.popularBooks = response;
-        console.log(this.popularBooks);
-      },
-    });
+    this.getPopularBooksByGenre('non fiction');
   }
   //this method is to navigate to a new page with the book id as a parameter so it can be used in the book-details component
   openBookDetails(id: string): void {
@@ -43,18 +34,21 @@ export class HomeComponent implements OnInit {
   getPopularBooksByGenre(genre: string) {
     this.bookService.getWeeksPopularBooks(genre).subscribe({
       next: (response) => {
-        console.log(response);
         this.popularBooks = response;
-        console.log(this.popularBooks);
       },
     });
   }
 
   addToFav(name: string) {
     this.isFav = true;
-    this.favouritesService.addFavourite(name).subscribe((response: any) => {
-      console.log(response);
-    });
+    this.favouritesService.addFavourite(name).subscribe(
+      (response: any) => {
+        console.log(response);
+      },
+      (error) => {
+        console.log('error adding to favourites: ', error);
+      }
+    );
     this.snackbar.openSnackBar(`Added ${name} to favourites`, '');
   }
 }
