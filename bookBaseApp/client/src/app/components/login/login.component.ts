a0c840bdf7f6ae5dcb735b6cd2dce13dc0fac415
conor <conorlyness@gmail.com>
2022-08-17 23:06:37 +0100

    new login comp and initial work

--- bookBaseApp/client/src/app/components/login/login.component.ts
+++ bookBaseApp/client/src/app/components/login/login.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthenticateService } from 'src/app/services/authenticate.service';
+import { SnackbarService } from 'src/app/services/snackbar.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
+  
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required, Validators.minLength(6)]);
+  isValidLogin!: any;
+
+  constructor(    
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private snackbarService: SnackbarService,
+    private authService: AuthenticateService,) { }
 
   ngOnInit(): void {
   }
+
+  onSubmit() {
+    if (!this.email.valid || !this.password.valid) {
+      console.log("error")
+      this.snackbarService.openSnackBar("Invalid fields, please retry","")
+    } else {
+      console.log("form submitted: ", this.email.value, this.password.value);
+      const user = {email: this.email.value, password: this.password.value}
+      //add some logic here, make a call to the auth service and a func to check if there is a user in the db with this email already, if so then 
+      //output an error message in a snackbar "account with this email already exists"
+      
+        this.authService.loginUser(user).subscribe((value) => {
+          this.isValidLogin = value;
+          if (this.isValidLogin.userId) {
+              this.snackbarService.openSnackBar("Login Successful, welcome back","");
+              this.router.navigate(['']);
+            }
+            else {
+              this.password.reset();
+              this.snackbarService.openSnackBar("Invalid credentials, please retry","");
+            }
+
+        });
+      
+      }
+    }
 
 }
