a57b7ac57047d323c8a31a78bde7e66107d91d4a
conor <conorlyness@gmail.com>
2022-08-17 18:44:00 +0100

    new auth service to login user/call endpoint

---
+++ bookBaseApp/client/src/app/services/authenticate.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticateService {
+
+  constructor(private http: HttpClient) { }
+
+  createUser(user: object) {
+    const url = environment.createNewUserUrl;
+    return this.http.post<Observable<any>>(url, user);
+  }
+}
